d671103cfbcb9a7b970942b6208c9ad298f545fe
James <jamesvogelgesang23@gmail.com>
2024-01-03 19:39:11 -0800

    Add review date and review date function

--- classes.py
+++ classes.py
@@ -1,21 +1,11 @@
+import datetime
+
 class Problem:
-    """A simple class"""
     def __init__(self, number, difficulty, date, review):
-        self.data = {"number":number, "difficulty":difficulty, "date_completed":date, "review":review}
         self.number = number
         self.difficulty = difficulty
         self.date = date
         self.review = review
 
-
-def date_parse(date):
-    date_specifics = date.split('/')
-    month = date_specifics[0]
-    day = date_specifics[1]
-    year = date_specifics[2]
-    return month, day, year
-
-
-# prob = problem(12, 3, "1/1/2024", "ASAP")
-
-# print(date_parse(prob.date))
+    def review_calc(self):
+        return self.date + datetime.timedelta(days=(self.review)-1)
